9c5661847913abda221d46b59f33cba0de97c746
suman-infomaticae <suman.mondal@infomaticae.co.in>
2025-06-25 23:30:26 +0530

    mutual fund save api integration

--- src/pages/FundDetailPage.tsx
+++ src/pages/FundDetailPage.tsx
@@ -1,18 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import Header from "../components/Header";
+import { useUser } from "../context/UserContext";
+import axios from "axios";
+import ToastMessage from "../components/ToastMessage";
 
 const FundDetailPage: React.FC = () => {
   const { state } = useLocation() as any;
   const fund = state?.fund;
   const navigate = useNavigate();
+  const { user } = useUser();
 
-  const handleSave = () => {
-    const savedFunds = JSON.parse(localStorage.getItem("savedFunds") || "[]");
-    savedFunds.push(fund);
-    localStorage.setItem("savedFunds", JSON.stringify(savedFunds));
-    alert("Fund saved!");
-    navigate("/saved-funds");
+  const [toastMessage, setToastMessage] = useState<string>("");
+  const [toastType, setToastType] = useState<"success" | "danger">("success");
+  const [showToast, setShowToast] = useState<boolean>(false);
+
+  const showToastHandler = (message: string, type: "success" | "danger") => {
+    setToastMessage(message);
+    setToastType(type);
+    setShowToast(true);
+    setTimeout(() => setShowToast(false), 3000);
+  };
+
+  const handleSave = async () => {
+    //const savedFunds = JSON.parse(localStorage.getItem("savedFunds") || "[]");
+    // savedFunds.push(fund);
+    // localStorage.setItem("savedFunds", JSON.stringify(savedFunds));
+    // alert("Fund saved!");
+
+    const payload = {
+      schemeCode: fund.schemeCode,
+      schemeName: fund.schemeName,
+      userId: user?._id,
+    };
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/api/v1/save-mutual-fund",
+        payload
+      );
+
+      if (response.status === 201) {
+        showToastHandler(response.data.message, "success");
+      } else if (response.status === 409) {
+        showToastHandler(response.data.message, "danger");
+      }
+    } catch (error) {
+      showToastHandler("Something went wrong!", "danger");
+    }
+
+    //navigate("/saved-funds");
   };
 
   const handleBack = () => {
@@ -52,6 +88,11 @@ const FundDetailPage: React.FC = () => {
             </button>
           </div>
         </div>
+        <ToastMessage
+          message={toastMessage}
+          type={toastType}
+          show={showToast}
+        />
       </div>
     </>
   );
